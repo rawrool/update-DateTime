25ce4d1e9a5b906d2809e254d0c27de5d64a9008
rawrool <raul.herrera.cs@gmail.com>
2019-10-13 22:55:42 -0700

    Add class WebSite to retrieve site information

--- updatedatetime.py
+++ updatedatetime.py
@@ -1,10 +1,31 @@
-import datetime
+from datetime import datetime, timezone
+from urllib.error import URLError
 from urllib.request import urlopen
-res = urlopen('http://just-the-time.appspot.com/')
-result = res.read().strip()
-siteHeaders = res.info()
-headerDict = dict(res.info())
-print(headerDict.get("Date"))
-# print(result.decode('utf-8'))
-# print(datetime.datetime.now())
-# print(datetime.datetime.utcfromtimestamp())
+
+site = urlopen('http://just-the-time.appspot.com/')
+siteHeaders = site.info()
+headerDict = dict(siteHeaders)
+dateString = headerDict.get("Date")
+dateStringFormatted = dateString.replace(",", "")
+dateTimeGMT = datetime.strptime(dateStringFormatted, '%a %d %b %Y %X %Z')
+dateTimeLocal = dateTimeGMT.replace(tzinfo=timezone.utc).astimezone(tz=None)
+print(dateTimeLocal)
+
+
+class WebSite:
+    def __init__(self, url):
+        self.siteUrl = url
+        self.siteResponse = self.get_site_response()
+
+    def get_site_response(self):
+        try:
+            self.siteResponse = urlopen(self.siteUrl)
+        except URLError as e:
+            if hasattr(e, 'reason'):
+                print('We failed to reach the site.')
+                print('Reason: ', e.reason)
+            elif hasattr(e, 'code'):
+                print('The server couldn\'t fulfill the request.')
+                print('Error code: ', e.code)
+        else:
+            return self.siteResponse
